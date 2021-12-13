d882f11af7a8d93bb66fae7a956cc8830109ea50
Quinton Chester <quintonchester@gmail.com>
2021-12-13 00:26:34 -0600

    Need to solidify tests prior to releasing. Very close to completion.

--- __tests__/ProgressiveImage.js
+++ __tests__/ProgressiveImage.js
@@ -2,16 +2,49 @@ import React from "react";
 import { render } from "@testing-library/react-native";
 import ProgressiveImage from "../src";
 
-describe("ProgressiveImage", () => {
+describe("AnimatableProgressiveImage", () => {
     jest.useFakeTimers();
     const tree = (
         <ProgressiveImage
             small_source={{ uri: "https://i.imgur.com/T8bP9tw.jpg" }}
             large_source={{ uri: "https://i.imgur.com/v74Ah44.jpg" }}
+            animation_library="animatable"
         />
     );
 
-    it("should render correctly", () => {
+    it("Animatable Progressive Image should render correctly", () => {
+        const page = render(tree);
+        expect(page.toJSON()).toMatchSnapshot();
+    });
+});
+
+describe("AnimatedProgressiveImage", () => {
+    jest.useFakeTimers();
+    const tree = (
+        <ProgressiveImage
+            small_source={{ uri: "https://i.imgur.com/T8bP9tw.jpg" }}
+            large_source={{ uri: "https://i.imgur.com/v74Ah44.jpg" }}
+            animation_library="animated"
+        />
+    );
+
+    it("Animated API should render correctly", () => {
+        const page = render(tree);
+        expect(page.toJSON()).toMatchSnapshot();
+    });
+});
+
+describe("ReanimatedProgressiveImage", () => {
+    jest.useFakeTimers();
+    const tree = (
+        <ProgressiveImage
+            small_source={{ uri: "https://i.imgur.com/T8bP9tw.jpg" }}
+            large_source={{ uri: "https://i.imgur.com/v74Ah44.jpg" }}
+            animation_library="reanimated"
+        />
+    );
+
+    it("Reaimated API should render correctly", () => {
         const page = render(tree);
         expect(page.toJSON()).toMatchSnapshot();
     });
--- src/props.tsx
+++ src/props.tsx
@@ -6,13 +6,29 @@ import {
     ViewStyle,
 } from "react-native";
 
+// Packages
+import * as Animatable from "react-native-animatable";
+import { WithTimingConfig } from "react-native-reanimated";
+
 export default interface Props {
     small_source: ImageProps["source"];
     large_source: ImageProps["source"];
-    type?: "spring" | "timing";
-    style?: StyleProp<ViewStyle>;
-    timing_config: Animated.TimingAnimationConfig;
-    spring_config: Animated.SpringAnimationConfig;
     initial_blur_radius?: ImagePropsIOS["blurRadius"];
     use_native_driver?: boolean;
+    style?: StyleProp<ViewStyle>;
+    animation_duration?: number;
+    // Animatable Props
+    animation_in?: Animatable.Animation;
+    animation_out?: Animatable.Animation;
+    // Animated Propss
+    type?: "spring" | "timing";
+    timing_config?: Animated.TimingAnimationConfig;
+    spring_config?: Animated.SpringAnimationConfig;
+    // Reanimated Props
+    in_easing?: WithTimingConfig["easing"];
+    out_easing?: WithTimingConfig["easing"];
+}
+
+export interface CoreProps extends Props {
+    animation_library?: "animatable" | "animated" | "reanimated";
 }
